fcc8d26fcfcb8eb6adb2783606d0cad6ef1b8a46
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-14 23:47:29 +0900

    なぜか行けてしまった #1

--- src/config/initializers/cors.rb
+++ src/config/initializers/cors.rb
@@ -7,4 +7,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 		expose: ["access-token", "expiry", "token-type", "uid", "client"], # 追記
 		methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
+	end
   end
