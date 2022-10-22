5e8263dd2c4350f0baeaef3a2e5b41fb5abe680d
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-22 11:54:27 +0900

    seed作成

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,1 +1,8 @@
-User.create!(name: 'テストユーザー', email: 'test-user+1@example.com', password: 'password')
+User.create!(
+	name: 'テストユーザー',
+	provider:'email',
+	uid: '1@1.com',
+	email: '1@1.com',
+	password: '111111',
+	encrypted_password:'$2a$12$Y0PvxG/30ix56RnA09m5UuwW2qOKc3hzvN1ulpja3GFPAmGlwu7NG',
+	tokens:{"5A1p9257SUmD1WqDfWbsGw":{"token":"$2a$10$LO3oJ9N6Yqli7aSP.EJIM.iNQ2XIp5I/6wK4LdOv0nasL1KaVnJtO","expiry":1667570466}})
